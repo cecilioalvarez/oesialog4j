0770665a311132996e6e84cfa0640a5dfe9a054d
cecilioAlvarez <cmac@arquitecturajava.com>
2023-10-24 13:56:12 +0200

    suido

--- logs/src/main/java/es/oesia/logs/Principal2.java
+++ logs/src/main/java/es/oesia/logs/Principal2.java
@@ -1,8 +1,9 @@
 package es.oesia.logs;
 
-
-
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import es.oesia.app1.models.Persona;
 import es.oesia.app1.repositories.PersonaRepository;
@@ -10,15 +11,28 @@ import es.oesia.app1.services.PersonaServices;
 
 public class Principal2 {
 
+	private static final Logger milogger = LogManager.getLogger(Principal2.class);
+
 	public static void main(String[] args) {
 
-		
-		PersonaRepository repo= new PersonaRepository();
-		PersonaServices servicio= new PersonaServices(repo);
-		List<Persona> lista= servicio.buscarTodos();
-		
+		for (int i = 0; i < 10; i++) {
+
+			milogger.info("app principal");
+		}
+
+		PersonaRepository repo = new PersonaRepository();
+		PersonaServices servicio = new PersonaServices(repo);
+		List<Persona> lista = servicio.buscarTodos();
+
+		for (int i = 0; i < 10; i++) {
+
+			milogger.info("app principal");
+		}
 		lista.forEach(System.out::println);
-		
+		for (int i = 0; i < 10; i++) {
+
+			milogger.info("app principal");
+		}
 	}
 
 }
